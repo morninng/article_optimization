652ab45f08f777a223d104d9fef0df5f00913b8f
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-02-22 21:45:02 +0900

    first commit 2

---
+++ practice5_app.js
@@ -0,0 +1,29 @@
+var ContactManager = new Marionette.Application();
+
+ContactManager.addRegions({
+  mainRegion: "#main-region"
+});
+
+
+ContactManager.navigate = function(route, options){
+	options || (options = {});
+	Backbone.history.navigate(route, options);
+};
+
+
+ContactManager.getCurrentRoute = function(){
+	return Backbone.history.fragment
+};
+
+
+ContactManager.on("start", function(){
+
+	if(Backbone.history){
+		Backbone.history.start();
+
+		if(this.getCurrentRoute() === ""){
+			ContactManager.trigger("contacts:list");
+		}
+	}
+});
+
